7d7d6feeeb35f34906c6d1fbc68d0390d0e89084
Kai Geissdoerfer <kai.geissdoerfer@tu-dresden.de>
2023-06-22 11:05:53 +0200

    client: pop methods and proper exception handling

--- host/riotee_gateway/client.py
+++ host/riotee_gateway/client.py
@@ -1,8 +1,10 @@
 import requests
 import numpy as np
 import base64
+from typing import List
 
 from riotee_gateway.packet_model import PacketApiSend
+from riotee_gateway.packet_model import PacketApiReceive
 
 
 def decode_dev_id(dev_id_b64: str):
@@ -30,7 +32,7 @@ class GatewayClient(object):
 
         return _convert_dev_id_wrapped
 
-    def get_devices(self):
+    def get_devices(self) -> List[str]:
         """Reads the list of all devices from which the gateway has received packets."""
         r = requests.get(f"{self.__url}/devices")
         r.raise_for_status()
@@ -47,37 +49,61 @@ class GatewayClient(object):
         self.send_packet(dev_id, pkt)
 
     @convert_dev_id
-    def get_queue_size(self, dev_id: int | str):
+    def get_queue_size(self, dev_id: int | str) -> int:
         """Reads the number of packets in the queue for the corresponding device."""
         r = requests.get(f"{self.__url}/in/{dev_id}/size")
         r.raise_for_status()
-        return r.json()
+        return int(r.json())
 
     @convert_dev_id
-    def get_packet(self, dev_id: int | str, pkt_index: int):
-        """Retrieves a packet from the gateways fifo queue."""
+    def get_packet(self, dev_id: int | str, pkt_index: int) -> PacketApiReceive:
+        """Retrieves a packet from the gateway's fifo queue."""
         r = requests.get(f"{self.__url}/in/{dev_id}/{pkt_index}")
         r.raise_for_status()
-        return r.json()
+
+        return PacketApiReceive.from_json(r.json())
 
     @convert_dev_id
     def delete_packet(self, dev_id: int | str, pkt_index: int):
-        """Retrieves a packet from the gateways fifo queue."""
+        """Retrieves a packet from the gateway's fifo queue."""
         r = requests.delete(f"{self.__url}/in/{dev_id}/{pkt_index}")
         r.raise_for_status()
         return r.json()
 
     @convert_dev_id
-    def get_packets(self, dev_id: int | str = None):
+    def pop(self, dev_id: int | str) -> PacketApiReceive:
+        """Pops the first packet from the gateways' fifo queue for the specified device.."""
+        try:
+            pkt = self.get_packet(dev_id, 0)
+        except requests.HTTPError as e:
+            if e.response.status_code == 404:
+                raise IndexError("No packets in Queue for device")
+            raise
+        self.delete_packet(dev_id, 0)
+        return pkt
+
+    @convert_dev_id
+    def pops(self, dev_id: int | str):
+        """Pops all packets from the gateway's fifo queue for the specified device."""
+        while True:
+            try:
+                yield self.pop(dev_id)
+            except IndexError:
+                return
+
+    @convert_dev_id
+    def get_packets(self, dev_id: int | str = None) -> List[PacketApiReceive]:
+        """Retrieves all packets from all the gateway's fifo queues."""
         if dev_id is None:
             r = requests.get(f"{self.__url}/in/all/all")
         else:
             r = requests.get(f"{self.__url}/in/{dev_id}/all")
         r.raise_for_status()
-        return r.json()
+        return [PacketApiReceive.from_json(json_dict) for json_dict in r.json()]
 
     @convert_dev_id
     def delete_packets(self, dev_id: int | str = None):
+        """Deletes all packets from all the gateway's fifo queues."""
         if dev_id is None:
             r = requests.delete(f"{self.__url}/in/all/all")
         else:
